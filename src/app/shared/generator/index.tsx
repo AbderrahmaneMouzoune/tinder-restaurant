d0bd920fd96fb8866f0ddd39a6b9ae8138d36614
AbderrahmaneMouzoune <abderrahmane.mouzoune@gmail.com>
2024-05-23 20:39:53 +0200

    generate form

--- src/app/shared/generator/index.tsx
+++ src/app/shared/generator/index.tsx
@@ -1,25 +1,62 @@
+'use client'
 import { Button } from '@/components/ui/button'
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
+import { zodResolver } from '@hookform/resolvers/zod'
 import { LocateIcon } from 'lucide-react'
 import React from 'react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+const formSchema = z.object({
+  address: z.string().min(5).max(100),
+})
 
 export default function Generator() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  })
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values)
+  }
+
   return (
-    <form className="max-w-md w-full bg-white dark:bg-gray-800 rounded-lg shadow-lg p-6 space-y-4 mx-auto my-5">
-      <div className="grid gap-2">
-        <Label htmlFor="location">Localisation</Label>
-        <div className="flex flex-col gap-2">
-          <Input id="location" placeholder="Enter your location" type="text" />
-          <Button variant="outline">
-            <LocateIcon className="size-5 mr-2" />
-            Ou utilisé ma position
+    <section className="max-w-md w-full bg-white dark:bg-gray-800 rounded-lg shadow-lg p-6 mx-auto my-5">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2">
+          <FormField
+            control={form.control}
+            name="address"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Localisation</FormLabel>
+                <FormControl>
+                  <Input placeholder="Entrer votre localisation" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button className="w-full" type="submit">
+            Générer la liste de restaurants
           </Button>
-        </div>
-      </div>
-      <Button className="w-full" type="submit">
-        Générer la liste de restaurants
+        </form>
+      </Form>
+      <Button variant="outline" className="w-full mt-5">
+        <LocateIcon className="size-5 mr-2" />
+        Ou utilisé ma position
       </Button>
-    </form>
+    </section>
   )
 }
