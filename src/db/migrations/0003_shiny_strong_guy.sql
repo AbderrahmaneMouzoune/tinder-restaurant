c4ed09fa7e63acdb90a357e9e374675387423b44
AbderrahmaneMouzoune <abderrahmane.mouzoune@gmail.com>
2024-05-31 22:09:02 +0200

    feat: add relation to participant

---
+++ src/db/migrations/0003_shiny_strong_guy.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS "participants" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"userName" text NOT NULL,
+	"avatar" text NOT NULL
+);
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,4 @@
-import { InferInsertModel, InferSelectModel } from 'drizzle-orm'
+import { InferInsertModel, InferSelectModel, relations } from 'drizzle-orm'
 import { pgTable, serial, text } from 'drizzle-orm/pg-core'
 
 export const rooms = pgTable('rooms', {
@@ -7,5 +7,25 @@ export const rooms = pgTable('rooms', {
   shareCode: text('shareCode').unique().notNull(),
 })
 
+export const roomsRelations = relations(rooms, ({ many }) => ({
+  participants: many(participants),
+}))
+
+export const participants = pgTable('participants', {
+  id: serial('id').primaryKey(),
+  userName: text('userName').notNull(),
+  avatar: text('avatar').notNull(),
+})
+
+export const participantsRelations = relations(participants, ({ one }) => ({
+  roomAuthor: one(rooms, {
+    fields: [participants.userName],
+    references: [rooms.id],
+  }),
+}))
+
 export type Room = InferSelectModel<typeof rooms>
 export type NewRoom = InferInsertModel<typeof rooms>
+
+export type Participant = InferSelectModel<typeof participants>
+export type NewParticipant = InferInsertModel<typeof participants>
